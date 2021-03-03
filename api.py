cc065f93faa02a5069e9721a92ffb0f3cb17f9c7
Jacob Sanders <sanders.j4@gmail.com>
2021-03-03 12:39:25 -0500

    implemented transactions route

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ import db
 
 import flask
 from flask import request, jsonify, make_response
+import time
 from uuid import uuid4
 
 
@@ -184,6 +185,76 @@ def transactions_all():
     return jsonify(results)
 
 
-@app.route('/api/v1/transactions', methods=['GET'])
-def transactions_filter():
-    pass
+@app.route('/api/v1/transactions', methods=['GET', 'POST'])
+def transactions():
+    if request.method == 'GET':
+        query_parameters = request.args
+
+        id = query_parameters.get('id')
+        buyer_id = query_parameters.get('buyer_id')
+        offer_id = query_parameters.get('offer_id')
+        status = query_parameters.get('status')
+        start_timestamp = query_parameters.get('start_timestamp')
+        end_timestamp = query_parameters.get('end_timestamp')
+
+        query = 'SELECT * FROM transactions WHERE'
+        to_filter = []
+
+        if id:
+            query += ' id=? AND'
+            to_filter.append(id)
+        if buyer_id:
+            query += ' seller_id=? AND'
+            to_filter.append(buyer_id)
+        if offer_id:
+            query += ' offer_id=? AND'
+            to_filter.append(offer_id)
+        if status:
+            query += ' status=? AND'
+            to_filter.append(status)
+        if start_timestamp:
+            query += ' start_timestamp=? AND'
+            to_filter.append(start_timestamp)
+        if end_timestamp:
+            query += ' end_timestamp=? AND'
+            to_filter.append(buyer_id)
+        if not (id or buyer_id or offer_id or status or start_timestamp or
+                end_timestamp):
+            return page_not_found(404)
+
+        query = query[:-4] + ';'
+
+        with db.connect() as conn:
+            conn.row_factory = dict_factory
+            cur = conn.cursor()
+            results = cur.execute(query, to_filter).fetchall()
+
+        return jsonify(results)
+    else:
+        data = request.get_json()
+
+        try:
+            buyer_id = data['buyer_id']
+            offer_id = data['offer_id']
+            #status = int(data['status'])
+            #start_timestamp = data['start_timestamp']
+            #end_timestamp = data['end_timestamp']
+        except AttributeError:
+            return make_response(400)
+
+        id = uuid4().hex
+        tx = (id, buyer_id, offer_id, 'PENDING', int(time.time()), None)
+
+        query = ''' INSERT INTO transactions(
+                        id,
+                        buyer_id,
+                        offer_id,
+                        status,
+                        start_timestamp,
+                        end_timestamp)
+                    VALUES (?, ?, ?, ?, ?, ?)'''
+
+        with db.connect() as conn:
+            conn.execute(query, tx)
+
+        return make_response(jsonify('Success'), 200)
