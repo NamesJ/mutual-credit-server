c002dbdeada87ab7b3482c8a138cf660ca173da8
J <sanders.j4@gmail.com>
2021-03-05 23:28:04 -0500

    Fixed marshmallow dependency issue

--- app/models/schemas.py
+++ app/models/schemas.py
@@ -4,13 +4,13 @@ from app import ma
 from .user import User
 
 
-class UserSchema(ma.ModelSchema):
+class UserSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         # Fields to expose, add more if needed.
         fields = ("email", "name", "username", "joined_date", "role_id")
 
 
-class TransferSchema(ma.ModelSchema):
+class TransferSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         fields = ('id', 'sender', 'receiver', 'value', 'memo', 'status',
                 'opened_on', 'closed_on')
