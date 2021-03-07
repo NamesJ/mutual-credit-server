455dc9c1ca4d4c1fd40b8ab797aad030a1480642
J <sanders.j4@gmail.com>
2021-03-06 22:37:35 -0500

    Added unittest for getting user account

--- tests/test_user_api.py
+++ tests/test_user_api.py
@@ -3,7 +3,9 @@ import json
 from flask_jwt_extended import create_access_token
 
 from app import db
+from app.models.account import Account
 from app.models.user import User
+from app.models.user_account import UserAccount
 
 from tests.utils.base import BaseTestCase
 from tests.utils.common import register_user, login_user
@@ -14,6 +16,15 @@ def get_user_data(self, access_token, username):
         f"/api/user/{username}",
         headers={"Authorization": f"Bearer {access_token}"},
         content_type="application/json",
+    )
+
+
+def get_user_account_data(self, access_token, username):
+    return self.client.get(
+        f"/api/user/accounts",
+        headers={"Authorization": f"Bearer {access_token}"},
+        content_type="application/json",
+        data=json.dumps({ 'username': username }),
     )
 
 
@@ -34,9 +45,46 @@ class TestUserBlueprint(BaseTestCase):
         user_data = json.loads(user_resp.data.decode())
 
         self.assertTrue(user_resp.status)
-        self.assertEquals(user_resp.status_code, 200)
-        self.assertEquals(user_data["user"]["username"], username)
+        self.assertEqual(user_resp.status_code, 200)
+        self.assertEqual(user_data["user"]["username"], username)
 
         # Test a 404 request
         user_404_resp = get_user_data(self, access_token, "non.existent")
-        self.assertEquals(user_404_resp.status_code, 404)
+        self.assertEqual(user_404_resp.status_code, 404)
+
+
+    def test_user_account_get(self):
+        ''' Test getting accounts for a user from DB '''
+
+        # Create a mock user
+        username = 'alice'
+        user = User(username=username)
+
+        db.session.add(user)
+        db.session.commit()
+
+        # Create a mock account
+        account = Account(allowance=100)
+
+        db.session.add(account)
+        db.session.commit()
+
+        # Create a mock user-account association
+        user_account = UserAccount(user_id=user.id, account_id=account.id)
+
+        db.session.add(user_account)
+        db.session.commit()
+
+        access_token = create_access_token(identity=user.id)
+
+        user_account_resp = get_user_account_data(self, access_token, username)
+        user_account_data = json.loads(user_account_resp.data.decode())
+
+        self.assertTrue(user_account_resp.status)
+        self.assertEqual(user_account_resp.status_code, 200)
+        self.assertEqual(user_account_data['user']['username'], username)
+        self.assertEqual(len(user_account_data['user']['accounts']), 1)
+        account_data = user_accounts_data['user']['accounts'][0]
+        self.assertEqual(account_data['id'], account.id)
+        self.assertEqual(account_data['balance'], account.balance)
+        self.assertEqual(account_data['allowance'], account.allowance)
