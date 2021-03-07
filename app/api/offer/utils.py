e92c89304efbbe1ec485b687e67b204bedf8ecc2
J <sanders.j4@gmail.com>
2021-03-07 04:37:51 -0500

    Closes #7

---
+++ app/api/offer/utils.py
@@ -0,0 +1,57 @@
+# Validations from Marshmallow
+from marshmallow import Schema, fields
+from marshmallow.validate import Regexp, Length, Range, OneOf
+
+
+def load_data(offer_db_obj):
+    """ Load offer's data
+
+    Parameters:
+    - Offer db object
+    """
+    from app.models.schemas import OfferSchema
+
+    offer_schema = OfferSchema()
+
+    data = offer_schema.dump(offer_db_obj)
+
+    return data
+
+
+class OfferCreateSchema(Schema):
+    """ /offer [POST]
+
+    Parameters:
+    - Title (Str)
+    - Price (Int)
+    - Description (Str)
+    """
+
+    title = fields.Str(required=False, validate=[Length(max=128)])
+    price = fields.Int(validate=[Range(min=1)])
+    description = fields.Str(required=False, validate=[Length(max=4000)])
+
+
+class OfferGetSchema(Schema):
+    """ /offer [GET]
+
+    Parameters:
+    - Id (Int)
+    """
+
+    id = fields.Int(required=False, validate=[Range(min=1)])
+
+
+
+class OfferUpdateSchema(Schema):
+    """ /offer [PUT]
+
+    Parameters:
+    - Title (Str)
+    - Price (Int)
+    - Description (Str)
+    """
+
+    title = fields.Str(required=False, validate=[Length(max=128)])
+    price = fields.Int(validate=[Range(min=1)])
+    description = fields.Str(required=False, validate=[Length(max=4000)])
