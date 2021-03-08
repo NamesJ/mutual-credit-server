96b9bb827dff9c6f6189ed833e1f91b72136fcb6
J <sanders.j4@gmail.com>
2021-03-07 20:38:38 -0500

    Closes #5 - implemented offer api unittests

--- tests/test_offer_api.py
+++ tests/test_offer_api.py
@@ -10,32 +10,30 @@ from tests.utils.base import BaseTestCase
 from tests.utils.common import register_user, login_user
 
 
-def create_offer(self, access_token, data):
+def create_offer(self, access_token, payload):
     return self.client.post(
         f'/api/offer',
         headers={'Authorization': f'Bearer {access_token}'},
         content_type='application/json',
-        data=json.dumps(data)
+        data=json.dumps(payload)
     )
 
 
-def get_offer_data(self, access_token, data):
-    return self.client.post(
+def get_offer_data(self, access_token, payload):
+    return self.client.get(
         f'/api/offer',
         headers={'Authorization': f'Bearer {access_token}'},
         content_type='application/json',
-        data=json.dumps(data)
+        data=json.dumps(payload)
     )
 
 
-def update_offer(self, access_token, data):
+def update_offer_info(self, access_token, payload):
     return self.client.put(
-        return self.client.post(
-            f'/api/offer',
-            headers={'Authorization': f'Bearer {access_token}'},
-            content_type='application/json',
-            data=json.dumps(data)
-        )
+        f'/api/offer',
+        headers={'Authorization': f'Bearer {access_token}'},
+        content_type='application/json',
+        data=json.dumps(payload)
     )
 
 
@@ -43,14 +41,131 @@ def update_offer(self, access_token, data):
 class TestOfferBlueprint(BaseTestCase):
     def test_offer_create(self):
         ''' Test creating an offer in DB '''
-        pass
+
+        # Create a mock user (seller)
+        username = 'ClaustrophobicSkeleton'
+        user = User(username=username)
+
+        db.session.add(user)
+        db.session.commit()
+
+        access_token = create_access_token(identity=user.id)
+
+        title = 'SpOOoOoky cake'
+        price = 30
+        description = '1x spOOoOoky 9x13x2in cake'
+
+        payload = dict(title=title, price=price, description=description)
+
+        offer_response = create_offer(self, access_token, payload)
+        offer_data = json.loads(offer_response.data.decode())
+
+        self.assertTrue(offer_response.status)
+        self.assertEqual(offer_response.status_code, 200)
+        self.assertIsNotNone(offer_data['offer']['id'])
+        self.assertEqual(offer_data['offer']['seller'], user.username)
+        self.assertEqual(offer_data['offer']['title'], title)
+        self.assertEqual(offer_data['offer']['price'], price)
+        self.assertEqual(offer_data['offer']['description'], description)
+        # self.assertIsNotNone(offer_data['offer']['created_on'])
+
 
 
     def test_offer_get(self):
         ''' Test getting an offer in DB '''
-        pass
+
+        # Create a mock user (seller)
+        username = 'ClaustrophobicSkeleton'
+        user = User(username=username)
+
+        db.session.add(user)
+        db.session.commit()
+
+        access_token = create_access_token(identity=user.id)
+
+        # Create a mock offer
+        seller = username
+        title = 'SpOOoOoky cake'
+        price = 30
+        description = '1x spOOoOoky 9x13x2in cake'
+        offer = Offer(seller=seller, title=title, price=price,
+                      description=description)
+
+        db.session.add(offer)
+        db.session.commit()
+
+        payload = dict(id=offer.id)
+
+        offer_response = get_offer_data(self, access_token, payload)
+        offer_data = json.loads(offer_response.data.decode())
+
+        self.assertTrue(offer_response.status)
+        self.assertEqual(offer_response.status_code, 200)
+        self.assertEqual(offer_data['offer']['id'], offer.id)
+        self.assertEqual(offer_data['offer']['seller'], seller)
+        self.assertEqual(offer_data['offer']['title'], title)
+        self.assertEqual(offer_data['offer']['price'], price)
+        self.assertEqual(offer_data['offer']['description'], description)
+        #self.assertIsNotNone(offer_data['offer']['created_on'])
 
 
     def test_offer_update(self):
         ''' Test updating offer in DB '''
-        pass
+
+        # Create a mock user (seller)
+        username = 'ClaustrophobicSkeleton'
+        user = User(username=username)
+
+        db.session.add(user)
+        db.session.commit()
+
+        access_token = create_access_token(identity=user.id)
+
+        # Create a mock offer
+        seller = username
+        title = 'SpOOoOoky cake'
+        price = 15
+        description = '1x spOOoOoky 9x13x2in cake'
+        offer = Offer(seller=seller, title=title, price=price,
+                      description=description)
+
+        db.session.add(offer)
+        db.session.commit()
+
+        # Update a single value
+        new_price = 30
+
+        payload = dict(id=offer.id, price=new_price)
+
+        offer_response = update_offer_info(self, access_token, payload)
+        offer_data = json.loads(offer_response.data.decode())
+
+        self.assertTrue(offer_response.status)
+        self.assertEqual(offer_response.status_code, 200)
+        self.assertEqual(offer_data['offer']['id'], offer.id)
+        self.assertEqual(offer_data['offer']['seller'], seller)
+        self.assertEqual(offer_data['offer']['title'], title)
+        self.assertEqual(offer_data['offer']['price'], new_price)
+        self.assertEqual(offer_data['offer']['description'], description)
+        #self.assertIsNotNone(offer_data['offer']['created_on'])
+
+
+        # Update multiple values
+        new_title = 'Extra SpOOoOoky cake'
+        new_price = 45
+        new_description = '1x extra spOOoOoky 9x13x2in cake'
+
+        payload = dict(id=offer.id, title=new_title, price=new_price,
+                       description=new_description)
+
+        offer_response = update_offer_info(self, access_token, payload)
+        offer_data = json.loads(offer_response.data.decode())
+
+        self.assertTrue(offer_response.status)
+        self.assertEqual(offer_response.status_code, 200)
+        self.assertEqual(offer_data['offer']['id'], offer.id)
+        self.assertEqual(offer_data['offer']['seller'], seller)
+        self.assertEqual(offer_data['offer']['title'], new_title)
+        self.assertEqual(offer_data['offer']['price'], new_price)
+        self.assertEqual(offer_data['offer']['description'], new_description)
+        #self.assertIsNotNone(offer_data['offer']['created_on'])
