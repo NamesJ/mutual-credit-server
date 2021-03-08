dcaaf1780f4ae0c803ce492989a5d8e687fc396a
J <sanders.j4@gmail.com>
2021-03-07 19:25:09 -0500

    Added test case, functions added but not implemented

--- tests/test_offer_api.py
+++ tests/test_offer_api.py
@@ -17,3 +17,40 @@ def create_offer(self, access_token, data):
         content_type='application/json',
         data=json.dumps(data)
     )
+
+
+def get_offer_data(self, access_token, data):
+    return self.client.post(
+        f'/api/offer',
+        headers={'Authorization': f'Bearer {access_token}'},
+        content_type='application/json',
+        data=json.dumps(data)
+    )
+
+
+def update_offer(self, access_token, data):
+    return self.client.put(
+        return self.client.post(
+            f'/api/offer',
+            headers={'Authorization': f'Bearer {access_token}'},
+            content_type='application/json',
+            data=json.dumps(data)
+        )
+    )
+
+
+
+class TestOfferBlueprint(BaseTestCase):
+    def test_offer_create(self):
+        ''' Test creating an offer in DB '''
+        pass
+
+
+    def test_offer_get(self):
+        ''' Test getting an offer in DB '''
+        pass
+
+
+    def test_offer_update(self):
+        ''' Test updating offer in DB '''
+        pass
